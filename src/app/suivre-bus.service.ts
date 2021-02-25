e2a3cd47fbd885a541c3098b12e177a246d1f098
Arraf YASSER <yasserarraf011@gmail.com>
2021-02-25 16:39:37 +0100

    setting up all tasks

--- src/app/suivre-bus.service.ts
+++ src/app/suivre-bus.service.ts
@@ -9,23 +9,26 @@ export class SuivreBusService {
 
   constructor(private http: HttpClient) {}
 
+  public host:string = "http://localhost:8080"
+
   public getAllstations(){
+    let url = this.host + '/stations'
     return this.http.get("assets/stations.json");
+    //return this.http.get(url);
   }
 
   public getAllLignes(){
+    let url = this.host + '/lignes'
     return this.http.get("assets/lignes.json");
+    //return this.http.get(url);
   }
-  public getAllBusLocations(){
+  public getAllBusLocations(url,data){
     return this.http.get("assets/busLocations.json");
+    //return this.http.post(url,data);
   }
-
-  public getMeilleurVoyage(){
-    return this.http.get("assets/meilleurChemin1.json");
-  }
-
 
   postRessource(url: any, data: any) {
     return this.http.get(url);
+    //return this.http.post(url,data);
   }
 }
